3b6537eaf7b1f79fc4a37f8c4860ce2465a38472
Scott Frazier <scottfrazier17@yahoo.com>
2023-10-19 23:16:25 -0500

    This is the new base for sprint 3.

--- A Wizards Tale/Scenes/Credits.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func _on_CreditsBack_pressed():
-  $"../UIPressCredits".play()
-  TransitionLayer.transition("res://Scenes/MainMenu.tscn")
-
-func _on_CreditsBack_mouse_entered():
-  $"../UIHoverCredits".play()
