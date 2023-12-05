50b53ca55f5c224335c9e10845f713fa9dd83cb2
ShafinBarshan <143758165+ShafinBarshan@users.noreply.github.com>
2023-12-05 09:29:06 -0600

    deleting old directories

--- A-Team-Game-Design-(26 Nov)/A Wizards Tale/Hitboxes and Hurtboxes/Stats.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-export var MAX_HEALTH = 4
-onready var CURRENT_HEALTH = MAX_HEALTH
--- A-Team-Game-Design-(26 Nov)/A Wizards Tale/Scenes/UICollectible.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-onready var label = $Label
-var collected_so_far = 0
-var max_collectibles = 10
-func _process(delta):
-  collected_so_far = World_Info.collected_amt
-  if(label):
-    label.text = str(collected_so_far) + " / " + str(max_collectibles)
